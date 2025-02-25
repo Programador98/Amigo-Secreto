7d63e0db70268ff2c36121777b39f85dff4e5b50
Vicente <mecanisimoaaron98@gmail.com>
2025-02-25 11:09:57 -0500

    Actualizar funcion Agregar Amigo

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var listaAmigos = [];
 function agregarAmigo(){
     let nombre = document.getElementById('amigo').value;
 
-    if(nombre = ''){
+    if(nombre == ''){
         alert('Por favor, ingrese un nombre correcto');
     } else{
         listaAmigos.push(nombre);
