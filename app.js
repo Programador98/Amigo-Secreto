3b039282de19412126c0b1269bd51708a07b6a76
Vicente <mecanisimoaaron98@gmail.com>
2025-02-25 13:20:42 -0500

    Agregar funcion Actualizar lista y Limpiar caja

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var listaAmigos = [];
 
+
+//Agregamos los nombres de los amigos a el arreglo.
 function agregarAmigo(){
     let nombre = document.getElementById('amigo').value;
 
@@ -7,6 +9,25 @@ function agregarAmigo(){
         alert('Por favor, ingrese un nombre correcto');
     } else{
         listaAmigos.push(nombre);
+        actualizarLista();
         console.log(listaAmigos);
+        limpiarCaja();
+    }
+}
+
+//Limpia el input
+function limpiarCaja(){
+    document.getElementById('amigo').value="";
+}
+
+//Actualiza y muestra los nombres en formato de lista
+function actualizarLista() {
+    let ul = document.getElementById("listaAmigos");
+    ul.innerHTML = ""; 
+
+    for (let i = 0; i < listaAmigos.length; i++) {
+        let li = document.createElement("li"); 
+        li.textContent = listaAmigos[i]; 
+        ul.appendChild(li); 
     }
 }
